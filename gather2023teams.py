fafb5628a8d5ee1e42c5196efc8787b5fc0ea68c
ericwlumsden <airique21@gmail.com>
2023-03-15 16:22:03 -0700

    added men's team ids

--- gather2023teams.py
+++ gather2023teams.py
@@ -9,7 +9,7 @@ import urllib.parse
 """ 
     Starting with Men's 2023 tournament teams
     Will gather that data using requests and bs4
-
+"""
 url = "https://www.sbnation.com/college-basketball/2023/3/12/23636762/2023-ncaa-tournament-full-rankings-alabama-houston-kansas-purdue"
 
 page = requests.get(url)
@@ -17,11 +17,9 @@ soup = BeautifulSoup(page.text, 'html')
 
 teams = soup.find('p', {'id':'wc1TA4'}).text
 teams_array = teams.split('. ')
-"""
 
 """
 Women's tournament teams will be scraped with the wikipedia pandas extension
-"""
 import wikipedia
 
 html = wp.page("2023 NCAA Division I women's basketball tournament").html().encode("UTF-8")
@@ -29,28 +27,30 @@ html = wp.page("2023 NCAA Division I women's basketball tournament").html().enco
 teams_array = []
 df = pd.read_html(html)[5]
 teams_array.append(df[1][2:].values)
+"""
 
-kaggle_womens_ids = pd.read_csv('./WTeams.csv')
+kaggle_mens_ids = pd.read_csv('./MTeams.csv')
 
-womens_teams_clean = {}
-womens_teams_missing = []
-for team in teams_array[0]:
+mens_teams_clean = {}
+mens_teams_missing = []
+for team_name in teams_array[1:]:
+    team = team_name.split(" (")[0]
     try:
-        #print(kaggle_womens_ids.query(f"TeamName=={clean_team_name}")["TeamID"])
+        #print(kaggle_mens_ids.query(f"TeamName=={clean_team_name}")["TeamID"])
 
-        temp_team_id = kaggle_womens_ids[kaggle_womens_ids["TeamName"].str.match(team)]
+        temp_team_id = kaggle_mens_ids[kaggle_mens_ids["TeamName"].str.match(team)]
         print(temp_team_id.iloc[0]["TeamID"])
-        womens_teams_clean[temp_team_id.iloc[0]["TeamID"]] = team
+        mens_teams_clean[temp_team_id.iloc[0]["TeamID"]] = team
     except:
-        womens_teams_missing.append(team)
+        mens_teams_missing.append(team)
         #print(clean_team_name)
 
-print(womens_teams_missing)
-for team in womens_teams_missing:
+print(mens_teams_missing)
+for team in mens_teams_missing:
     print(team)
     team_id = input("What is this team's ID?")
-    womens_teams_clean[team_id] = team
+    mens_teams_clean[team_id] = team
 
-final_teams_ids = pd.DataFrame(womens_teams_clean.items(), columns=['TeamID', 'TeamName'])
+final_teams_ids = pd.DataFrame(mens_teams_clean.items(), columns=['TeamID', 'TeamName'])
 
-final_teams_ids.to_csv('./2023WteamIDs.csv', index=False)
+final_teams_ids.to_csv('./2023MteamIDs.csv', index=False)
